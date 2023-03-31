dd322627b63f0c8ffeb7415358317834d79343fe
Arthur <arthurchen115@gmail.com>
2023-03-31 16:21:05 +0800

    half finish with intergrating the IR sensor

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,11 @@ const int frontRightPin = 33;
 const int frontLeftPin = 32;
 const int backRightPin = 22;
 const int backLeftPin = 23;
+// Pins for IR sensors
+// The ADC driver API supports ADC1 (8 channels, attached to GPIOs 32 - 39)
+// and ADC2 (10 channels, attached to GPIOs 0, 2, 4, 12 - 15 and 25 - 27)
+const int IN_A0 = 19; // analog input
+const int IN_D0 = 18; // digital input
 
 // Servo objects
 Servo frontRight;
@@ -33,24 +38,36 @@ void setup() {
   attachAndSetServo(frontLeft, frontLeftPin);
   attachAndSetServo(backRight, backRightPin);
   attachAndSetServo(backLeft, backLeftPin);
+
+  pinMode (IN_A0, INPUT);
+  pinMode (IN_D0, INPUT);
 }
 
+
+int value_A0;
+bool value_D0;
+
 void loop() {
-  Serial.println("Moving forward:");
-  moveForward(2000);
+
+  value_A0 = analogRead(IN_A0); // reads the analog input from the IR distance sensor
+  value_D0 = digitalRead(IN_D0);// reads the digital input from the IR distance sensor
+
+  Serial.println("Moving backward:");
+  moveBackward(500);
   delay(2000);
 
   Serial.println("Pausing for 2 seconds.");
   pause_motor();
-  delay(2000);
+  delay(2000);  
   
-    Serial.println("Moving backward:");
-  moveBackward(1000);
+  
+  Serial.println("Moving forward:");
+  moveForward(1000);
   delay(2000);
 
   Serial.println("Pausing for 2 seconds.");
   pause_motor();
-  delay(2000);
+  delay(2000);  
 
   Serial.println("Turning right:");
   turnRight(500);
@@ -60,13 +77,18 @@ void loop() {
   pause_motor();
   delay(2000);
 
+
   Serial.println("Turning left:");
   turnLeft(500);
   delay(2000);
 
   Serial.println("Pausing for 2 seconds.");
   pause_motor();
-  delay(2000);
+  delay(2000);  
+  
+
+
+
   
 
 }
@@ -80,30 +102,30 @@ void pause_motor() {
 }
 
 void moveForward(int pulseWidth) {
-  frontRight.writeMicroseconds(pulseWidth);
-  frontLeft.writeMicroseconds(pulseWidth);
-  backRight.writeMicroseconds(pulseWidth);
-  backLeft.writeMicroseconds(pulseWidth);
+  frontRight.writeMicroseconds(1500 + pulseWidth);
+  frontLeft.writeMicroseconds(1500 - pulseWidth);
+  backRight.writeMicroseconds(1500 + pulseWidth);
+  backLeft.writeMicroseconds(1500 - pulseWidth);
 }
 
 void moveBackward(int pulseWidth) {
-  frontRight.writeMicroseconds(pulseWidth);
-  frontLeft.writeMicroseconds(pulseWidth);
-  backRight.writeMicroseconds(pulseWidth);
-  backLeft.writeMicroseconds(pulseWidth);
-}
-
-void turnRight(int pulseWidth) {
   frontRight.writeMicroseconds(1500 - pulseWidth);
   frontLeft.writeMicroseconds(1500 + pulseWidth);
   backRight.writeMicroseconds(1500 - pulseWidth);
   backLeft.writeMicroseconds(1500 + pulseWidth);
 }
 
+void turnRight(int pulseWidth) {
+  frontRight.writeMicroseconds(1500 + pulseWidth);
+  frontLeft.writeMicroseconds(1500 + pulseWidth);
+  backRight.writeMicroseconds(1500 + pulseWidth);
+  backLeft.writeMicroseconds(1500 + pulseWidth);
+}
+
 
 void turnLeft(int pulseWidth) {
-  frontRight.writeMicroseconds(1500 + pulseWidth);
+  frontRight.writeMicroseconds(1500 - pulseWidth);
   frontLeft.writeMicroseconds(1500 - pulseWidth);
-  backRight.writeMicroseconds(1500 + pulseWidth);
+  backRight.writeMicroseconds(1500 - pulseWidth);
   backLeft.writeMicroseconds(1500 - pulseWidth);
 }
