eeecaa9f275fd4e633fc54a82d326563f9f8d7f2
jeffrey <jeffrey20110224@gmail.com>
2023-04-07 16:53:27 +0800

    code review

--- include/constantDefinition.h
+++ include/constantDefinition.h
@@ -10,8 +10,8 @@ const int backLeftPin = 23;
 // Pins for IR sensors
 // The ADC driver API supports ADC1 (8 channels, attached to GPIOs 32 - 39)
 // and ADC2 (10 channels, attached to GPIOs 0, 2, 4, 12 - 15 and 25 - 27)
-const int IN_A0 = 32; // analog input
-const int IN_A1 = 33; // analog input
+const int IN_A0 = 34; // analog input
+const int IN_A1 = 35; // analog input
 
 const int IN_D0 = 18; // digital input
 const int IN_D1 = 19; // digital input
