51617244eda4bdfdb0a6d4d29bab92711aa43ed0
UndertheCea <91667268+UndertheCea@users.noreply.github.com>
2023-04-21 22:42:44 +0800

    Update lift.cpp
    just for writing project I will commit the corrct code tomorow

--- src/lift.cpp
+++ src/lift.cpp
@@ -20,9 +20,7 @@ void setup() {
 	myservo1.attach(servo1Pin, 500, 2500); // attaches the servo on pin 18 to the servo object
 	myservo2.setPeriodHertz(50);    // standard 50 hz servo
 	myservo2.attach(servo2Pin, 500, 2500);
-	myservo3.setPeriodHertz(50);    // standard 50 hz servo
-	myservo3.attach(servo3Pin, 500, 2500);
-	// using default min/max of 1000us and 2000us
+	// using default min/max of 500us and 2500us
 	// different servos may require different min/max settings
 	// for an accurate 0 to 180 sweep
 }
@@ -34,19 +32,10 @@ void loop() {
 	// front = 18 back = 19
 	// for the front motor its upper height is limited by pos = 0 and lower height is limited by pos = 55
 	
-	for (pos = -7; pos <= 55; pos += 1) { // goes from 0 degrees to 180 degrees
-		// in steps of 1 degree
-		myservo1.write(pos);
-		myservo3.write(pos);
-		myservo2.write(0.9 * (65-pos));    // 65 is the shifting for rear motor the larger, the larger the 65, the higher the rear motor shift  
-		delay(150);             // waits 15ms for the servo to reach the position
-	}
-
-	delay(3000);
-	
 	for (pos = 55; pos >= -5; pos -= 1) { // goes from 180 degrees to 0 degrees
 		myservo1.write(pos);
 		myservo3.write(pos);
+		// 65 is the shifting for rear motor the larger, the larger the 65, the higher the rear motor shift  
 		myservo2.write(0.9*(65 - pos));    // tell servo to go to position in variable 'pos'
 		delay(150);             // waits 15ms for the servo to reach the position
 	}
